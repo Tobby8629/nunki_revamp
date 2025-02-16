b1324722800edee2e52ab29fd5dbe7b50807d794
Tekhlay <teklaybirhane12@yahoo.com>
2025-02-16 14:45:55 +0300

    Update Telemedicine page

--- src/assets/data.js
+++ src/assets/data.js
@@ -4,7 +4,6 @@ import {
   shackproduct,
   success,
   telemed,
-  
 } from "../../public/images/productImage/ProductImage";
 import { Dark } from "/public/images/dark/Dark";
 import { light } from "/public/images/light/Light";
@@ -170,24 +169,26 @@ export const products = [
         id: 1,
         benefit: "Accept Nunki Terms & conditions",
       },
-      { 
-        id: 2, 
-        benefit: "Pay for your Telemedicine Product" 
+      {
+        id: 2,
+        benefit: "Pay for your Telemedicine Product",
       },
-      { 
-        id: 3, 
-        benefit: 'Store Your Personalized token you receive safe it should look like "NUN838391"', 
-        img: success
+      {
+        id: 3,
+        benefit:
+          'Store Your Personalized token you receive safe it should look like "NUN838391"',
+        img: success,
       },
       {
         id: 4,
-        benefit: "Follow the link to our Telemedicine product where you can create your digital medical ID.",
+        benefit:
+          "Follow the link to our Telemedicine product where you can create your digital medical ID.",
       },
       {
         id: 5,
         benefit:
           "The rest is handled for you and you can start using the product",
-      }
+      },
     ],
   },
 ];
@@ -528,8 +529,6 @@ export const TelemedInputs = [
     page: "gender",
   },
 ];
-
-
 
 // benefits: [
 //   {
