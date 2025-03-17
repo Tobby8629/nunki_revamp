e7454931cb41ea69e939f48fcbb7da5a5eaf4fce
tobby8629 <sarhmullah15@gmail.com>
2025-03-17 01:39:22 -0500

    Fix coiming soon

--- src/pages/ContactUs.jsx
+++ src/pages/ContactUs.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import  { useState } from "react";
 import Swal from "sweetalert2";
 import template from "../component/products/template.module.css";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
